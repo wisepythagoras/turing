f0a8866331459213ec12eaf886af4619c2c9e6a9
Diogenis <55108282+wisepythagoras@users.noreply.github.com>
2023-08-20 18:22:06 -0400

    Add expression placeholder

--- src/parser.zig
+++ src/parser.zig
@@ -38,6 +38,10 @@ pub fn Parser() type {
             }
         }
 
+        pub fn expression(self: *Self) void {
+            _ = self;
+        }
+
         fn grouping(self: *Self) void {
             _ = self;
         }
